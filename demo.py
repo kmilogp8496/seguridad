b440cdf0d5c2319de3838da31944133bb38e5ec0
Kmilo Glez <kmilogp8496@gmail.com>
2023-05-17 18:49:30 +0200

    improvements

--- demo.py
+++ demo.py
@@ -15,41 +15,29 @@ for i in range(10):
     random_number = random.randint(0, 1000)
 
     get_key_data = json.dumps({"id": DEVICE_ID, "random": random_number})
-
     client = ServerClient(DEVICE_ID)
-
     response = client.generate_key(get_key_data)
 
     decoded_text = b64decode(response.text)
-
     encrypter = AESDemo(GENERATOR_SHARED_KEY)
-
     kas = json.loads(encrypter.decrypt(decoded_text))
 
     if kas["idB"] == SERVER_ID and kas["randomA"] == random_number:
         kab: str = kas["key"]
 
     new_key = b64decode(kab)
+    data = json.dumps({"temperature": random.randint(0, 30)})
+    
     chacha = ChaChaDemo(new_key)
-
-    temperature = random.randint(0, 30)
-
-    data = json.dumps({"temperature": temperature})
-
     r = client.chacha(chacha.encrypt(data.encode()))
-
     print("ChaCha response: " + r.text)
 
     fernet = FernetDemo(kab)
-
     r = client.fernet(fernet.encrypt(data.encode()))
-
     print("Fernet response: " + r.text)
 
     aes = AESDemo(new_key)
-
     r = client.fernet(fernet.encrypt(data.encode()))
-
     print("AES response: " + r.text)
     
     sleep(2)
